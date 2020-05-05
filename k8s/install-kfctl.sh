491faa2fb0e5660cb0105f54eb736b1e537c2166
carlomazzaferro <carlo.mazzaferro@gmail.com>
2020-05-05 18:13:08 +0200

    Add chmod and libcompat to enable kfctl usage

--- k8s/install-kfctl.sh
+++ k8s/install-kfctl.sh
@@ -5,4 +5,4 @@ echo "Downloading kfctl for $PLATFORM, version: $KF_VERSION"
 RELEASE=$(curl -s https://api.github.com/repos/kubeflow/kfctl/releases |
   jq -r ".[] | select(.name | contains(\"${KF_VERSION}\")) | .assets[] | select(.name | test(\"${PLATFORM}\")) | .browser_download_url")
 
-wget "${RELEASE}" && tar -xvf "${RELEASE##*/}" -C /usr/local/bin && rm -f "${RELEASE##*/}"
+wget "${RELEASE}" && tar -xvf "${RELEASE##*/}" -C /usr/local/bin && chmod +x /usr/local/bin/kfctl && rm -f "${RELEASE##*/}"
