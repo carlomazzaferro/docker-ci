4077b4734ff5ad63ca2003b1142c60ae4e951df9
carlomazzaferro <carlo.mazzaferro@gmail.com>
2020-05-05 17:09:42 +0200

    Kubectl-enabled docker image

---
+++ k8s/install-kfctl.sh
@@ -0,0 +1,8 @@
+set -eoux pipefail
+
+echo "Downloading kfctl for $PLATFORM, version: $KF_VERSION"
+
+RELEASE=$(curl -s https://api.github.com/repos/kubeflow/kfctl/releases |
+  jq -r ".[] | select(.name | contains(\"${KF_VERSION}\")) | .assets[] | select(.name | test(\"${PLATFORM}\")) | .browser_download_url")
+
+wget "${RELEASE}" && tar -xvf "${RELEASE##*/}" -C /usr/local/bin && rm -f "${RELEASE##*/}"
